3dc7d7ff09744985b6e0f1054842549eabdf0685
Hang Zhou <zhou_hang@berkeley.edu>
2019-09-27 21:38:53 -0700

    init pseudocode

---
+++ NN.java
@@ -0,0 +1,34 @@
+import Layer
+import NumJ
+
+public class NN
+{
+	private Array layers;
+
+	public static NN();
+
+	public static add_layer();
+	public static 
+	public static train(X, Y, learning_rate, iteration, verbose)
+	{
+		for(int i = 0; i < iteration; i++)
+		{
+			forward();
+			backpro();
+		}
+	}
+
+	private static forward(NumJ x_i)
+	{
+		NumJ h_i = x_i;
+		for(Layer layer : this.layers)
+		{
+			h_i = layer.forward(h_i);
+		}
+		return h_i
+	}
+	private static backpro();
+	{
+		
+	}
+}
---
+++ NumJ.java
@@ -0,0 +1,43 @@
+public class NumJ
+{
+	public static Array shape;
+
+	public static NumJ array(); // from primitve array
+	public static Array toArray(); // to primitive array
+
+
+	// shortcut init
+	public static NumJ zeros();
+	public static NumJ ones();
+
+
+	// airthmetic operations
+	public static NumJ add();
+	public static NumJ extract();
+	public static NumJ multiple();
+	public static NumJ divide()
+	public static NumJ 
+
+
+	// matrix operations
+	public static NumJ dot();
+	public static NumJ T();
+
+	// basic functions
+	public static NumJ log();
+	public static NumJ exp();
+	public static NumJ 
+	public static NumJ 
+
+	// util tools
+	public static NumJ sum();
+	public static NumJ max();
+	public static NumJ min();
+	public static NumJ random();
+
+
+	public static NumJ 
+
+
+
+}
